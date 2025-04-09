9c82f1d7d69e7363af780904555f79ad2649331a
Vijay Hardaha <vijayhardaha@gmail.com>
2025-04-09 05:56:20 +0530

    feat: update CORS headers to restrict allowed headers for improved security

--- src/app/api/couplets/route.js
+++ src/app/api/couplets/route.js
@@ -136,7 +136,7 @@ async function processRequest(params) {
 function addCorsHeaders(response) {
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET, POST");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
   return response;
 }
 
